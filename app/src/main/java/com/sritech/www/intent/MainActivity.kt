f732dc5a69c819205ec37b8ff835bdd4e4f827a4
sritechsoftsolutions <kotlasrikanth777@gmail.com>
2019-01-24 17:07:55 +0530

    intent example in kotlin

---
+++ app/src/main/java/com/sritech/www/intent/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.sritech.www.intent
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        b1.setOnClickListener{
+            var i=Intent()
+            i.action=Intent.ACTION_DIAL
+            //i.action="android.media.action.IMAGE_CAPTURE"
+            //i.action=Intent.ACTION_VIEW
+            startActivity(i)
+        }
+        b2.setOnClickListener{
+
+        }
+        b3.setOnClickListener{
+
+        }
+
+    }
+}
